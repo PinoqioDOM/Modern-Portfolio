fcc23e922bc467e6fa7f85274fc4595d44103d14
PinoqioDOM <alkhanishvilit@gmail.com>
2025-12-21 14:59:11 +0400

    ADD ROBOT

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,8 +8,7 @@ import {
 import { FiMapPin } from "react-icons/fi";
 import Galaxy from "./Galaxy";
 import Laptop from '../assets/laptop.png'
-import FirstSection from "./FirstSection";
-
+import Robot from "./Robot";
 
 export const SmoothScrollHero = () => {
   return (
@@ -110,15 +109,16 @@ const Schedule = () => {
   return (
     <section
       id="launch-schedule"
-      className="mx-auto max-w-5xl px-4 py-48 text-white"
+      className="mx-auto max-w-5xl px-4 py-12 text-white"
     >
+      
       <motion.h1
         initial={{ y: 48, opacity: 0 }}
         whileInView={{ y: 0, opacity: 1 }}
         transition={{ ease: "easeInOut", duration: 0.75 }}
-        className="mb-20 text-4xl font-black uppercase text-zinc-50"
+        className="flex items-center justify-center mb-20 text-7xl font-black uppercase text-zinc-50 "
       >
-        <FirstSection />
+        <Robot />
       </motion.h1>
 
 
---
+++ src/components/Robot.jsx
@@ -0,0 +1,46 @@
+import { useEffect } from 'react';
+
+const SplineScene = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.type = 'module';
+    script.src = 'https://unpkg.com/@splinetool/viewer@1.12.27/build/spline-viewer.js';
+    document.head.appendChild(script);
+
+    return () => {
+      if (document.head.contains(script)) {
+        document.head.removeChild(script);
+      }
+    };
+  }, []);
+
+  return (
+    <div style={{ width: '100%', height: '100vh' }}>
+        <h1 
+            className='text-center tracking-wider font-bold'
+            style={{
+                color: 'whitesmoke',
+                textShadow: `
+                0 0 5px #8a2be2,
+                0 0 10px #8a2be2,
+                0 0 20px #8a2be2,
+                0 0 40px #8a2be2,
+                0 0 80px #8a2be2
+                `,
+                WebkitTextStroke: '0.5px rgba(138, 43, 226, 0.7)'
+            }}
+        >
+        FRONT END DEVELOPER
+        <style>{`
+            @keyframes shine {
+            to { background-position: 100% center; }
+            }
+        `}</style>
+        </h1>
+      <spline-viewer url="https://prod.spline.design/v9lK0oj50Rs1HgTN/scene.splinecode">
+      </spline-viewer>
+    </div>
+  );
+}
+
+export default SplineScene;
